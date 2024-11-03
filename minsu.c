380df7ef183868b2a647a659ed6c55340dcd836e
chawoojin0425 <163067232+chawoojin0425@users.noreply.github.com>
2024-11-03 21:47:03 +0900

    Create minsu.c

---
+++ minsu.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        fprintf(stderr, "메모리 할당 실패\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addNode(Node** head, int data) {
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+void displayList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void deleteList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        Node* next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+int main() {
+    Node* head = NULL;
+
+    addNode(&head, 5);
+    addNode(&head, 15);
+    addNode(&head, 25);
+
+    printf("Linked List: ");
+    displayList(head);
+
+    deleteList(head);
+
+    return 0;
+}
